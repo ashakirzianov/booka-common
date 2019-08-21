e236c4ba2806ec97258ae106f8c084e12becde88
ashakirzianov <ashakirzianov@icloud.com>
2019-08-21 19:10:05 +0700

    Initial commit

---
+++ src/backContract.ts
@@ -0,0 +1,38 @@
+import { BookObject } from './bookFormat';
+import { BookCollection } from './libContract';
+
+export type AuthToken = {
+    token: string,
+};
+
+export type UserInfo = {
+    name: string,
+    pictureUrl?: string,
+};
+
+export type UserBooks = BookCollection;
+
+export type BackContract = {
+    '/auth/fbtoken': {
+        get: {
+            return: AuthToken,
+            query: { token: string },
+        },
+    },
+    '/me/info': { get: { return: UserInfo, auth: string } },
+    '/me/books': { get: { return: UserBooks, auth: string } },
+    '/book/single': {
+        get: {
+            return: BookObject,
+            query: { id: string },
+        },
+    },
+    '/book/all': { get: { return: BookCollection } },
+    '/book/upload': {
+        post: {
+            return: string,
+            files: 'book',
+            auth: string,
+        },
+    },
+};
---
+++ src/libContract.ts
@@ -0,0 +1,30 @@
+import { BookObject } from './bookFormat';
+
+export type BookInfo = {
+    id: string,
+    title: string,
+    author?: string,
+    cover?: string,
+};
+
+export type BookCollection = {
+    books: BookInfo[],
+};
+
+export type LibContract = {
+    '/single': {
+        get: {
+            query: { id: string },
+            return: BookObject,
+        },
+    },
+    '/all': {
+        get: { return: BookCollection },
+    },
+    '/upload': {
+        post: {
+            return: string,
+            files: 'book',
+        },
+    },
+};
