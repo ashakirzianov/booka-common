d26e01925b714b686bbff28486fd0f5d8f2e0ce4
ashakirzianov <ashakirzianov@icloud.com>
2019-08-21 21:49:48 +0700

    Remove all dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,4 @@ export * from './backContract';
 export * from './bookFormat';
 export * from './bookUtils';
 export * from './contractTypes';
-export * from './fetcher';
 export * from './libContract';
-export * from './mongooseUtils';
-export * from './router';
