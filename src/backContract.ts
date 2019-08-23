b0a6fe08c7336435fcbbd97e11702bdb69c1daed
ashakirzianov <ashakirzianov@icloud.com>
2019-08-23 16:55:34 +0700

    Update back contracts

--- src/backContract.ts
+++ src/backContract.ts
@@ -1,6 +1,7 @@
 import { BookObject } from './bookFormat';
 import { AuthToken, UserInfo, UserBooks } from './user';
 import { BookCollection } from './bookCollection';
+import { Highlight } from './highlights';
 
 export type BackContract = {
     '/auth/fbtoken': {
@@ -23,6 +24,32 @@ export type BackContract = {
             return: string,
             files: 'book',
             auth: string,
+        },
+    },
+    '/highlights': {
+        get: {
+            return: Highlight[],
+            auth: string,
+            query: {
+                bookId: string,
+            },
+        },
+        post: {
+            return: string,
+            auth: string,
+            query: {
+                bookId: string,
+                highlight: Highlight,
+            },
+        },
+        patch: {
+            return: boolean,
+            auth: string,
+            query: {
+                bookId: string,
+                highlightId: string,
+                highlight: Partial<Highlight>,
+            },
         },
     },
 };
