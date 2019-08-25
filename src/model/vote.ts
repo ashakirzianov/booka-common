4e1a5a19610fddfaf76070035ed35e8ea61dbf57
ashakirzianov <ashakirzianov@icloud.com>
2019-08-25 15:29:22 +0700

    Extend comment description

--- src/model/vote.ts
+++ src/model/vote.ts
@@ -1,7 +1,11 @@
+import { BookPath } from './bookRange';
+
 export type VoteKind = 'like' | 'dislike';
 
 export type CommentDescription = {
     commentId: string,
+    bookId: string,
+    path: BookPath[],
     textPreview: string,
 };
 export type Vote = {
