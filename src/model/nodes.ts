576d3cdaf3648cfc9b6df1aac5b017493d0ded22
ashakirzianov <ashakirzianov@icloud.com>
2019-09-05 14:33:18 +0700

    Introduce raw nodes

--- src/model/nodes.ts
+++ src/model/nodes.ts
@@ -1,5 +1,6 @@
 import { Span } from './span';
 import { BookRange } from './bookRange';
+import { KnownTag } from './tag';
 
 type DefNode<N extends string> = {
     node: N,
@@ -45,6 +46,26 @@ export type Quote = {
 export type QuoteNode = DefNode<'quote'> & {
     quote: Quote,
 };
+
+export type RefNode = DefNode<'ref'> & {
+    to: string,
+};
+export type TitleNode = DefNode<'title'> & {
+    title: string[],
+    level: number,
+};
+export type TagNode = DefNode<'tag'> & {
+    tag: KnownTag,
+};
+export type IgnoreNode = DefNode<'ignore'>;
+export type RawContainerNode = DefNode<'container'> & {
+    nodes: RawBookNode[],
+};
+export type RawBookNode =
+    | RefNode | TitleNode | TagNode | IgnoreNode
+    | RawContainerNode
+    | ParagraphNode | ImageNode
+    ;
 
 export type BookContentNode = ChapterNode | ParagraphNode | ImageNode;
 export type GeneratedContentNode = ParagraphNode | QuoteNode | ImageNode;
