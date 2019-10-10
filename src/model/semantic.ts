b39b30f1ce903eb0a83a25a7e6fc25a7eab66c0d
ashakirzianov <ashakirzianov@icloud.com>
2019-10-10 11:05:29 +0700

    More node semantics

--- src/model/semantic.ts
+++ src/model/semantic.ts
@@ -15,15 +15,28 @@ type Quote = DefSemantic<'quote', {
 type Epigraph = DefSemantic<'epigraph', {
     signature?: string[],
 }>;
-type FlagSemantic =
-    | DefSemantic<'poem'> | DefSemantic<'formated'>
+
+type TechNote = DefSemantic<'tech-note', {
+    source?: string,
+}>;
+
+export type FlagSemantic =
+    | DefSemantic<'poem'> | DefSemantic<'letter'>
+    | DefSemantic<'chapter-abstract'>
+    | DefSemantic<'character-name'>
+    | DefSemantic<'formated'> | DefSemantic<'side-note'>
     | DefSemantic<'editor-note'> | DefSemantic<'extracts'>
+    | DefSemantic<'footer'> | DefSemantic<'table-of-contents'>
+    | DefSemantic<'title-page'>
+    | DefSemantic<'illustrations'>
+    | DefSemantic<'junk'>
     ;
 
 export type SemanticKey = Semantic['semantic'];
 export type SemanticForKey<K extends SemanticKey> = Extract<Semantic, { semantic: K }>;
 
 export type Semantic =
+    | TechNote
     | Footnote | Correction
     | Quote | Epigraph
     | FlagSemantic
