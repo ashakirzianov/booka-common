6aa576a1450f48ab26b9a6e4a0f464ce52b6be82
ashakirzianov <ashakirzianov@icloud.com>
2019-10-30 14:31:34 +0700

    Support fragment length

--- src/utils/book.ts
+++ src/utils/book.ts
@@ -4,7 +4,7 @@ import {
 } from '../model';
 import { pathLessThan, nodesForRange, nodeForPath, emptyPath } from './bookRange';
 import {
-    extractNodeText, normalizeNodes, isEmptyContentNode, iterateNodes,
+    extractNodeText, normalizeNodes, isEmptyContentNode, iterateNodes, nodeLength,
 } from './bookNode';
 import { extractSpanText } from './span';
 
@@ -32,7 +32,7 @@ function* iterateTocItems(nodes: BookNode[]): IterableIterator<TableOfContentsIt
     }
 }
 
-export function fragmentForPath(book: Book, path: BookPath): BookFragment {
+export function fragmentForPath(book: Book, path: BookPath, fragmentLength?: number): BookFragment {
     type PathTitle = {
         path: BookPath,
         title: string | undefined,
@@ -44,12 +44,16 @@ export function fragmentForPath(book: Book, path: BookPath): BookFragment {
     };
     let next: PathTitle | undefined = undefined;
     let nodes: BookNode[] = [];
+    let currentLength = 0;
     let isUnderTitle = true;
     for (const [node, nodePath] of iterateNodes(book.nodes)) {
         if (node.node !== 'title' || isUnderTitle) {
             nodes.push(node);
             if (isUnderTitle && !isEmptyContentNode(node)) {
                 isUnderTitle = false;
+            }
+            if (fragmentLength) {
+                currentLength += nodeLength(node);
             }
             continue;
         }
@@ -58,13 +62,20 @@ export function fragmentForPath(book: Book, path: BookPath): BookFragment {
             title: node.title,
         };
         if (pathLessThan(path, nodePath)) {
-            next = pair;
-            break;
+            if (!fragmentLength || currentLength >= fragmentLength) {
+                next = pair;
+                break;
+            } else {
+                nodes.push(node);
+                currentLength += nodeLength(node);
+            }
+        } else {
+            previous = current;
+            current = pair;
+            isUnderTitle = true;
+            nodes = [];
+            currentLength = 0;
         }
-        previous = current;
-        current = pair;
-        isUnderTitle = true;
-        nodes = [];
     }
 
     return {
