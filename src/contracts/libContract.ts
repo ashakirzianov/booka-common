a412d547af0b521bd817feabd532e479dc704a5e
ashakirzianov <ashakirzianov@icloud.com>
2020-04-01 23:38:35 -0300

    Add '/toc' contract

--- src/contracts/libContract.ts
+++ src/contracts/libContract.ts
@@ -1,5 +1,5 @@
 import {
-    Book, LibraryCard, BookFragment, SearchResult, BookPath, CardCollection,
+    Book, LibraryCard, BookFragment, SearchResult, BookPath, CardCollection, TableOfContents,
 } from '../model';
 import { Paginate } from './helpers';
 
@@ -15,9 +15,15 @@ export type LibContract = {
         get: {
             return: { preview: string | undefined },
             query: {
-                bookId: string,
+                id: string,
                 node: number,
             },
+        },
+    },
+    '/toc': {
+        get: {
+            return: TableOfContents | undefined,
+            query: { id: string },
         },
     },
     '/fragment': {
